f11726c037b2fe0a9cd375686001c7258408d277
klill6506 <ken@thetaxshelter.com>
2025-09-18 18:24:09 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-import uvicorn
+from main import app
 
-if __name__ == '__main__':
-    uvicorn.run("app.main:app", host="0.0.0.0", port=5000, reload=False)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=False)
